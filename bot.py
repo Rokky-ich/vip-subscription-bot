03d8439b26839d5075e51a1ef0f9817340e69948
Rokky <acedragon15@gmail.com>
2025-07-22 13:59:28 +0300

    подвязал нового админа

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import os
 API_TOKEN = os.getenv("API_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))  # Должен начинаться с -100
 WEBHOOK_HOST = os.getenv("WEBHOOK_HOST")
-ADMIN_ID = 1279721354
+ADMIN_ID = 8052545896
 
 WEBHOOK_PATH = f"/webhook/{API_TOKEN}"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
